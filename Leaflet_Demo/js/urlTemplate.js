f7f62277a070546874b31a31045754b7c1a26f81
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2018-01-26 00:57:40 +0800

    esri-leaflet底图加载（底图类型、切换）

--- Leaflet_Demo/js/urlTemplate.js
+++ Leaflet_Demo/js/urlTemplate.js
@@ -1,6 +1,15 @@
  const urlTemplate = {
      mapbox_Image: "https://api.mapbox.com/styles/v1/yqcim/cizh1ma3400ez2so5x1anhuzo/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieXFjaW0iLCJhIjoiY2l6ZmhnZjExMDBhajJ4cGxnNGN5MnhpdCJ9.pcZtdfk8mSFboCdwqkvW6g",
      mapbox_Vector: "https://api.mapbox.com/styles/v1/liuvigongzuoshi/cjbf0ypqz8wp62ro6iayiivzy/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGl1dmlnb25nenVvc2hpIiwiYSI6ImNqYmYwd24wczJyd24yeGxsMWE5bGpxaGkifQ.CVKELjW5ASK8EnQfQCbxFw",
+    SYS_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_IMG/MapServer",
+     // 影像地图
+    SYS_CQMap_IMG_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_IMG/MapServer",
+     // 影像注记
+    SYS_CQMap_IMG_LABEL_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_IMG_LABEL/MapServer",
+     // 矢量地图
+    SYS_CQMap_VEC_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_VEC/MapServer",
+     // 地形地图
+    SYS_CQMap_DEM_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_DEM/MapServer",
     //自定义中国灰色地图（ArcGIS Server切片服务，来自arcgisonline.cn）
      chinagray: "http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineCommunity_Mobile/MapServer",
     //自定义中国彩色地图（ArcGIS Server切片服务，来自arcgisonline.cn）
