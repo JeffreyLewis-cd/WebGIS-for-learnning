8ddbb7946e977d1bfe779157598e79f9e5216a68
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2018-02-19 18:16:22 +0800

    change mapbox url

--- Leaflet_Demo/js/urlTemplate.js
+++ Leaflet_Demo/js/urlTemplate.js
@@ -1,6 +1,10 @@
  const urlTemplate = {
+     //在mapbox自定义的影像地图
      mapbox_Image: "https://api.mapbox.com/styles/v1/yqcim/cizh1ma3400ez2so5x1anhuzo/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieXFjaW0iLCJhIjoiY2l6ZmhnZjExMDBhajJ4cGxnNGN5MnhpdCJ9.pcZtdfk8mSFboCdwqkvW6g",
+     //在mapbox自定义的矢量地图
      mapbox_Vector: "https://api.mapbox.com/styles/v1/liuvigongzuoshi/cjbf0ypqz8wp62ro6iayiivzy/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGl1dmlnb25nenVvc2hpIiwiYSI6ImNqYmYwd24wczJyd24yeGxsMWE5bGpxaGkifQ.CVKELjW5ASK8EnQfQCbxFw",
+     //在mapbox地图接口
+     mapbox_url: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
     SYS_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_IMG/MapServer",
      // 影像地图
     SYS_CQMap_IMG_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_IMG/MapServer",
@@ -9,22 +13,5 @@
      // 矢量地图
     SYS_CQMap_VEC_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_VEC/MapServer",
      // 地形地图
-    SYS_CQMap_DEM_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_DEM/MapServer",
-    //自定义中国灰色地图（ArcGIS Server切片服务，来自arcgisonline.cn）
-     chinagray: "http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineCommunity_Mobile/MapServer",
-    //自定义中国彩色地图（ArcGIS Server切片服务，来自arcgisonline.cn）
-     chinacolor: "http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineStreetWarm/MapServer",
-    //天地图矢量地图服务
-    TianDiTuVec: "http://t{s}.tianditu.cn/DataServer?T=vec_w&X={x}&Y={y}&L={z}",
-    //天地图矢量标注服务
-    TianDiTuVec_A: "http://t{s}.tianditu.cn/DataServer?T=cva_w&X={x}&Y={y}&L={z}",
-    //天地图卫星底图服务
-    TianDiTuSat: "http://t{s}.tianditu.cn/DataServer?T=img_w&X={x}&Y={y}&L={z}",
-    //天地图卫星影像标注
-    TianDiTuSat_A: "http://t{s}.tianditu.cn/DataServer?T=cia_w&X={x}&Y={y}&L={z}",
-
-    //高德矢量地图服务
-    GaodeVec: "http://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}",
-    //高德矢量地图服务
-    GaodeVec_A: "http://webst0{s}.is.autonavi.com/appmaptile?style=8&x={x}&y={y}&z={z}"
+    SYS_CQMap_DEM_MAPSERVER_PATH: "http://www.digitalcq.com/RemoteRest/services/CQMap_DEM/MapServer"
 }
